95ed3878e47ffa695692021e7364012d33dfd946
KamilaLopatowska <kamilalopatowska@gmail.com>
2024-05-14 16:50:27 +0200

    2024-05-14 KŁ

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,41 @@
 from app import app
-from flask import render_template
-
-
+from flask import render_template, request, redirect, url_for
+from bs4 import BeautifulSoup
+import requests
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template("index.html.jinja")
+    return render_template('index.html.jinja')
 
-@app.route('/extract')
+
+@app.route('/extract', methods=['POST', 'GET'])
 def extract():
-    return render_template("extract.html.jinja")
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        url = f"https://www.ceneo.pl/{product_id}"
+        response = requests.get(url=url)
+        if response.status_code == requests.codes['ok']:
+            page_dom = BeautifulSoup(response.text, "html.parser")
+            try:
+                opinions_count = page_dom.select_one("a.product-review__link > span").get_text().strip()
+            except AttributeError:
+                opinions_count = 0
+            if opinions_count:
+                return redirect(url_for('product', product_id=product_id))
+            error = "Brak opinii :("
+            return render_template('extract.html.jinja', error=error)
+        error = "Błędny kod - strona nie istnieje :("
+        return render_template('extract.html.jinja', error=error)
+    return render_template('extract.html.jinja')
 
 @app.route('/products')
 def products():
-    return render_template("products.html.jinja")
+    return render_template('products.html.jinja')
 
 @app.route('/author')
 def author():
-    return render_template("author.html.jinja")
+    return render_template('author.html.jinja')
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    return render_template("product.html.jinja", product_id=product_id)
-
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name="World"):
-    return f"Hello, {name}!"
+    return render_template('product.html.jinja', product_id=product_id)
